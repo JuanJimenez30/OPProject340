9167683853eb413c7346bbe9e4b7b96fb1b5c5ff
Juan Jimenez Mora <j_jimenezmo@uncg.edu>
2025-10-23 14:08:43 -0400

    Added the last entity bases for subscription.

--- Backend API/src/main/java/com/example/Greensboro/Lawncare/Company/GLC/Subscription/SubscriptionRepository.java
+++ Backend API/src/main/java/com/example/Greensboro/Lawncare/Company/GLC/Subscription/SubscriptionRepository.java
@@ -1,5 +1,17 @@
 package com.example.Greensboro.Lawncare.Company.GLC.Subscription;
 
-public class SubscriptionRepository {
-    
+import com.example.Greensboro.Lawncare.Company.GLC.Customer.Customer;
+import com.example.Greensboro.Lawncare.Company.GLC.Provider.Provider;
+import com.example.Greensboro.Lawncare.Company.GLC.Services.Services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    List<Subscription> findByCustomerAndActive(Customer customer, boolean active);
+    List<Subscription> findByServices(Services services);
+    List<Subscription> findByServicesProvider(Provider provider);
 }
